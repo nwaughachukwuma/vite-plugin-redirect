bc431d5c5fb76bec8e63a388299030cceae3faf9
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-07-25 10:17:46 +0100

    add logic to call the middleware before internal middleware are installed

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,18 +1,28 @@
 import type { PluginOption } from 'vite';
 
-const redirect = (redirectMap: Record<string, string>): PluginOption => ({
+const redirect = (
+  redirectMap: Record<string, string>,
+  immediate: boolean = false
+): PluginOption => ({
   name: 'redirect',
-  configureServer: (server) => () =>
-    server.middlewares.use((req, res, next) => {
-      if (!!redirectMap[req.url]) {
-        res.statusCode = 302;
-        res.setHeader('Location', redirectMap[req.url]);
-        res.setHeader('Content-Length', '0');
-        res.end();
-      } else {
-        next();
-      }
-    })
+  configureServer: (server) => {
+    const returnFunc = () =>
+      server.middlewares.use((req, res, next) => {
+        if (!!redirectMap[req.url]) {
+          res.statusCode = 302;
+          res.setHeader('Location', redirectMap[req.url]);
+          res.setHeader('Content-Length', '0');
+          res.end();
+        } else {
+          next();
+        }
+      });
+
+    if (!immediate) {
+      return returnFunc;
+    }
+    returnFunc();
+  }
 });
 
 export default redirect;
